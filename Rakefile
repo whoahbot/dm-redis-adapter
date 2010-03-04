0f90b2786069f770f23f2a0b23eed762723428b9
Chris Hoffman <cehoffman@gmail.com>
2010-03-04 17:46:41 -0600

    Use bundler for managing what gems are required to develop

--- Rakefile
+++ Rakefile
@@ -1,4 +1,13 @@
-require 'rubygems'
+begin
+  # Try to require the preresolved locked set of gems.
+  require File.expand_path('../.bundle/environment', __FILE__)
+rescue LoadError
+  # Fall back on doing an unlocked resolve at runtime.
+  require "rubygems"
+  require "bundler"
+  Bundler.setup
+end
+
 require 'spec/rake/spectask'
 
 GEM = 'dm-redis-adapter'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,13 @@
-require 'rubygems'
+begin
+  # Try to require the preresolved locked set of gems.
+  require File.expand_path('../.bundle/environment', __FILE__)
+rescue LoadError
+  # Fall back on doing an unlocked resolve at runtime.
+  require "rubygems"
+  require "bundler"
+  Bundler.setup
+end
+
 require 'dm-core'
 
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib/dm_redis'))
