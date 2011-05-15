0182c984458c57be35a1213940e141d5e9879768
Dan Herrera <whoahbot@gmail.com>
2011-05-15 14:18:13 -0700

    adding in some unit tests for the data-store to prevent regression errors

--- spec/dm_redis_spec.rb
+++ spec/dm_redis_spec.rb
@@ -7,12 +7,33 @@ describe DataMapper::Adapters::RedisAdapter do
       :db => 15
     })
     @repository = DataMapper.repository(@adapter.name)
+    @redis = Redis.new(:db => 15)
   end
 
   it_should_behave_like 'An Adapter'
 
+  describe "adapter level tests" do
+    before(:all) do
+      class Hooloovoo
+        include DataMapper::Resource
+
+        property :id,      Serial
+        property :shade,   String, :index => true
+      end
+    end
+
+    it "should save the id of the resource in a set" do
+      h = Hooloovoo.create
+      @redis.smembers("hooloovoo:id:all").should == [h.id.to_s]
+    end
+
+    it "should save indexed fields in a set" do
+      h = Hooloovoo.create(:shade => '000080')
+      @redis.smembers("hooloovoo:shade:MDAwMDgw").should == [h.id.to_s]
+    end
+  end
+
   after(:all) do
-    redis = Redis.new(:db => 15)
-    redis.flushdb
+    @redis.flushdb
   end
 end
