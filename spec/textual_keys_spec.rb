9e0389ec61a73123b72d7a9be36ff5fc2b56bf65
Sebastian Feuerstack <Sebastian@Feuerstack.org>
2012-11-20 15:37:40 -0200

    fixed test to work with rake spec

--- spec/textual_keys_spec.rb
+++ spec/textual_keys_spec.rb
@@ -6,47 +6,46 @@ describe DataMapper::Adapters::RedisAdapter do
         :adapter  => "redis",
         :db => 15
     })
+
+    class Foo_1
+            include DataMapper::Resource
+            property :hostname,   Text, :key => true
+            property :ip_address, Text
+    end
+    class Foo
+          include DataMapper::Resource
+          property :key1,   String, :key => true
+          property :key2, String, :key => true
+        end
+          DataMapper.finalize
+
   end
 
-  after(:all) do
+  after(:each) do
     Redis.new(:db => 15).flushdb
   end
 
   describe "textual keys" do
     it "should return the key" do
-      class Foo
-        include DataMapper::Resource
-        property :hostname,   Text, :key => true
-        property :ip_address, Text
-      end
-      DataMapper.finalize
 
-      Foo.create(:hostname => "hostname1", :ip_address => '127.0.0.1')
 
-      Foo.first.hostname.should == "hostname1"
+      Foo_1.create(:hostname => "hostname1", :ip_address => '127.0.0.1')
 
-      Foo.first(:hostname => "hostname1").ip_address.should == "127.0.0.1"
+      Foo_1.first.hostname.should == "hostname1"
+
+      Foo_1.first(:hostname => "hostname1").ip_address.should == "127.0.0.1"
     end
 
     it "should find elements with two natural keys by querying with only one key" do
-      class Foo
-        include DataMapper::Resource
-        property :key1,   String, :key => true
-        property :key2, String, :key => true
-      end
-      DataMapper.finalize
+
+
 
       Foo.create(:key1 => "value1", :key2 => 'value2')
       Foo.first(:key1 => "value1").key2.should == 'value2'
     end
 
     it "should support direct key-based query with get using two natural keys" do
-      class Foo
-        include DataMapper::Resource
-        property :key1,   String, :key => true
-        property :key2, String, :key => true
-      end
-      DataMapper.finalize
+
 
       Foo.create(:key1 => "value1", :key2 => 'value2')
       Foo.get("value1","value2").key2.should == 'value2'
